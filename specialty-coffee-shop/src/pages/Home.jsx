559b32465198dffb64abf938a37cb18ce6987cca
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-01-25 16:37:31 +0100

    add home semantic structure (with placeholders), remove unnecessary code in App.sass

--- specialty-coffee-shop/src/pages/Home.jsx
+++ specialty-coffee-shop/src/pages/Home.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 
-const Home = () => {
+import Card from "../components/Card";
+
+const Home = ({viewport}) => {
     return (
-    <div>Das ist die HOME Seite</div>
+    <main>
+        <section>
+            <figure>
+                <img src="https://imageplaceholder.net/382" alt="Alessio Nocita, Gründer von Specialty Coffee Duisburg" />
+                <figcaption>Alessio Nocita, Gründer von Specialty Coffee Duisburg</figcaption>
+            </figure>
+            <article>
+            <h1>Hero-Titel</h1>
+            <p>Hero-Text</p>
+            </article>
+        </section>
+        <section>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </section>
+        <section>
+        <figure>
+                <img src="https://imageplaceholder.net/382" alt="Unser Kaffee, von uns mit Leidenschaft und Können geröstet" />
+                <figcaption>Unser Kaffee, von uns mit Leidenschaft und Können geröstet</figcaption>
+            </figure>
+            <article>
+            <h1>Unser Kaffee</h1>
+            <p>Kaffeetext</p>
+            </article>
+        </section>
+        <section>
+            <article>
+            <h1>Das neue Blog-Post</h1>
+                <figure>
+                    <img src="https://imageplaceholder.net/382" alt="Das neue Blog-Post" />
+                    <figcaption>Das neue Blog-Post</figcaption>
+                </figure>
+                <p>Das neue Blog-Post Text</p>
+            </article>
+        </section>
+    </main>
     );
     }
 
