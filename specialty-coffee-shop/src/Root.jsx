677d959c4fd6cd5face8210cca1abd9b5254aeb4
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-01-25 13:50:39 +0100

    upload logo asset, add root structure

--- specialty-coffee-shop/src/Root.jsx
+++ specialty-coffee-shop/src/Root.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
 import { Outlet } from "react-router-dom";
 
-const Root = () => {
+import MobileHeader from "./components/MobileHeader";
+import DesktopHeader from "./components/DesktopHeader";
+import Footer from "./components/Footer";
+
+const Root = ({viewport}) => {
     return (
     <>
+    {viewport < 1080 ? <MobileHeader /> : <DesktopHeader />}
     <Outlet />
+    <Footer />
     </>
     );
     }
