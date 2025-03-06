4703e6dde138a3dc513d1386f4385e2ec082bf59
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-03-06 22:05:22 +0100

    start Tailwind integration

---
+++ specialty-coffee-shop/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,scss,sass}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
