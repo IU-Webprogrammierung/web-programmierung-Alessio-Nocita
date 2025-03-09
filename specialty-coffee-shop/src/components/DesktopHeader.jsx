fd491138be1046249e0a0688b461d956b5569c5b
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-03-09 06:33:59 +0100

    delete DesktopHeader.module.sass

--- specialty-coffee-shop/src/components/DesktopHeader.jsx
+++ specialty-coffee-shop/src/components/DesktopHeader.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./DesktopHeader.module.sass";
 
 import logo from "../assets/images/specialty_coffee_duisburg_logo.png";
 
