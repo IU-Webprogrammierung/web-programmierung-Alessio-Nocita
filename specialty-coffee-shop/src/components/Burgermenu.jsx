192a130d667f5f4eba34df8bf894bb8fd0e05ac4
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-03-19 22:35:13 +0100

    refactor Burgermenu to Tailwind, delete Burgermenu.module.sass

--- specialty-coffee-shop/src/components/Burgermenu.jsx
+++ specialty-coffee-shop/src/components/Burgermenu.jsx
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
-import styles from "./Burgermenu.module.sass";
-
-import { useEffect } from "react";
+import React from "react";
 
 // Quelle für animiertes Burger Icon: https://codepen.io/designcouch/pen/ExvwPY
-// (für React angepasst)
+// (für React und Tailwind angepasst)
 
-const Burgermenu = ({mobileMenuOpen, toggler}) => {
-
-  const toggleMenu = () => {
-   toggler();
-  };
-
+const Burgermenu = ({ mobileMenuOpen, toggler }) => {
   return (
     <div
-      className={`${styles.navIcon} ${mobileMenuOpen ? styles.open : ""}`}
-      onClick={toggleMenu}
+      className="w-[80px] tablet:w-[50px] h-[40px] relative my-0 mx-auto z-[1000] block transform rotate-0 transition duration-500 ease-in-out cursor-pointer"
+      onClick={toggler}
       aria-label="Gehe zur mobilen Navigation"
     >
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      <span className={`block absolute h-[6px] w-full bg-[#333] rounded-md left-0 transition-all ease-in-out
+        ${mobileMenuOpen ? "top-[12px] w-[0px] left-[50%] bg-[#f0f0f0] duration-200 opacity-0" : "top-0 block opacity-100 duration-500"}
+      `}></span>
+
+      <span className={`block absolute h-[6px] w-full bg-[#333] rounded-md opacity-100 left-0 transition-all ease-in-out
+        ${mobileMenuOpen ? "rotate-45 top-[12px] bg-[#f0f0f0] duration-200" : "top-[12px] duration-500"}
+      `}></span>
+
+      <span className={`block absolute h-[6px] w-full bg-[#333] rounded-md opacity-100 left-0 transition-all ease-in-out
+        ${mobileMenuOpen ? "-rotate-45 top-[12px] bg-[#f0f0f0] duration-200" : "top-[12px] duration-500"}
+      `}></span>
+
+      <span className={`block absolute h-[6px] w-full bg-[#333] rounded-md left-0 transition-all  ease-in-out
+        ${mobileMenuOpen ? "top-[12px] w-[0px] left-[50%] bg-[#f0f0f0] duration-200 opacity-0" : "top-[24px] opacity-100 duration-500"}
+      `}></span>
     </div>
   );
 };
 
 export default Burgermenu;
-
