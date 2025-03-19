651bf1541544fb5dba9c81eb195af995fb35a05b
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-03-19 22:46:15 +0100

    refactor DesktopMenu to Tailwind, delete DesktopMenu.module.sass

--- specialty-coffee-shop/src/components/DesktopMenu.jsx
+++ specialty-coffee-shop/src/components/DesktopMenu.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import styles from "./DesktopMenu.module.sass";
-
 // Navigationsmenü für Desktop-Ansicht
 // enthält Links zu den Hauptseiten der Website
 
@@ -12,35 +10,35 @@ const DesktopMenu = () => {
             <ul className="flex justify-between my-10 mx-auto">
                 <li className="text-md active:text-md hover:text-md focus:text-md list-none">
                     <NavLink to="/" 
-                    className={({isActive}) => isActive ? styles.active : styles.inactive}
+                    className={({isActive}) => isActive ? "underline text-[#616b3a] text-2xl font-bold hover:text-red-800 focus:text-[#616b3a]" : "no-underline text-black text-2xl font-bold hover:text-red-800 focus:text-black"}
                     aria-label="Gehe zur Homepage" exact>
                         Home
                     </NavLink>
                 </li>
                 <li className="text-md active:text-md hover:text-md focus:text-md list-none">
                     <NavLink 
-                    className={({isActive}) => isActive ? styles.active : styles.inactive}
+                    className={({isActive}) => isActive ? "underline text-[#616b3a] text-2xl font-bold hover:text-red-800 focus:text-[#616b3a]" : "no-underline text-black text-2xl font-bold hover:text-red-800 focus:text-black"}
                     aria-label="Gehe zur 'Über uns'" to="/about">
                         Über uns
                     </NavLink>
                 </li>
                 <li className="text-md active:text-md hover:text-md focus:text-md list-none">
                     <NavLink 
-                    className={({isActive}) => isActive ? styles.active : styles.inactive}
+                    className={({isActive}) => isActive ? "underline text-[#616b3a] text-2xl font-bold hover:text-red-800 focus:text-[#616b3a]" : "no-underline text-black text-2xl font-bold hover:text-red-800 focus:text-black"}
                     aria-label="Gehe zu 'Unser Kaffee'" to="/coffee">
                         Unser Kaffee
                     </NavLink>
                 </li>
                 <li className="text-md active:text-md hover:text-md focus:text-md list-none">
                     <NavLink 
-                    className={({isActive}) => isActive ? styles.active : styles.inactive}
+                    className={({isActive}) => isActive ? "underline text-[#616b3a] text-2xl font-bold hover:text-red-800 focus:text-[#616b3a]" : "no-underline text-black text-2xl font-bold hover:text-red-800 focus:text-black"}
                     aria-label="Gehe zum Blog" to="/blog">
                         Blog
                     </NavLink>
                 </li>
                 <li className="text-md active:text-md hover:text-md focus:text-md list-none">
                     <NavLink 
-                    className={({isActive}) => isActive ? styles.active : styles.inactive}
+                    className={({isActive}) => isActive ? "underline text-[#616b3a] text-2xl font-bold hover:text-red-800 focus:text-[#616b3a]" : "no-underline text-black text-2xl font-bold hover:text-red-800 focus:text-black"}
                     aria-label="Gehe zum Kontaktformular" to="/contact">
                         Kontakt
                     </NavLink>
