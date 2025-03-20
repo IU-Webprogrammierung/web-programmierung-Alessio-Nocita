e418f5ac98d819b6c003f47a0bd3f88f0fc88579
Alessio-Nocita <alessio.nocita@iu-study.org>
2025-03-20 10:28:05 +0100

    implement Javascript Validation and button disable function

--- specialty-coffee-shop/src/components/Button.jsx
+++ specialty-coffee-shop/src/components/Button.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 // Button-Komponente
 // diese importiert aria-label, text und Verlinkung als props
 
-const Button = ({text, link, aria, color}) => {
+const Button = ({text, link, aria, color, formValid}) => {
+
+    const [disabled, setDisabled] = useState(false);
 
     useEffect(() => {
+        if (!formValid && typeof formValid!==undefined) {
+            setDisabled(true);
+        } else {
+            setDisabled(false);
+        }
         if (color !== "white") {
             color = "black";
         }
-    }, []);
+
+    }, [formValid]);
 
     const navigate = useNavigate();
 
@@ -22,7 +30,7 @@ const Button = ({text, link, aria, color}) => {
 
     return(
         <button
-            className={`button-${color}`}
+            className={`${disabled ? "button-disabled" : `button-${color}`}`}
             aria-label={aria} 
             tabIndex="0"
             onClick={handleClick}>
--- specialty-coffee-shop/src/pages/Contact.jsx
+++ specialty-coffee-shop/src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 import Button from "../components/Button";
 
@@ -7,6 +8,142 @@ import Button from "../components/Button";
 // (keine Backend-Anbindung)
 
 const Contact = () => {
+
+  const [firstNameInput, setFirstNameInput] = useState("");
+  const [lastNameInput, setLastNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [messageInput, setMessageInput] = useState("");
+
+  const [firstNameErrorMessage, setFirstNameErrorMessage] = useState("");
+  const [lastNameErrorMessage, setLastNameErrorMessage] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [messageErrorMessage, setMessageErrorMessage] = useState("");
+
+  const [isFirstNameTouched, setIsFirstNameTouched] = useState(false);
+  const [isLastNameTouched, setIsLastNameTouched] = useState(false);
+  const [isEmailTouched, setIsEmailTouched] = useState(false);
+  const [isMessageTouched, setIsMessageTouched] = useState(false);
+
+  const [isFormValid, setIsFormValid] = useState();
+
+  const nameRegex = /^[a-zA-Z-]+$/;
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const messageRegex = /^[a-zA-Z0-9-.,;:!?() ]+$/;
+
+  useEffect(() => {
+    const isValid =
+      firstNameErrorMessage === "" &&
+      lastNameErrorMessage === "" &&
+      emailErrorMessage === "" &&
+      messageErrorMessage === "" &&
+      firstNameInput !== "" &&
+      lastNameInput !== "" &&
+      emailInput !== "" &&
+      messageInput !== "";
+
+    setIsFormValid(isValid);
+  }, [
+    firstNameInput,
+    lastNameInput,
+    emailInput,
+    messageInput,
+    isFirstNameTouched,
+    isLastNameTouched,
+    isEmailTouched,
+    isMessageTouched,
+  ]);
+  
+  const validateFirstName = (firstName) => {
+    if (firstName === "") {
+      return "Bitte geben Sie Ihren Vornamen ein.";
+    } else if (firstName.length < 2) {
+      return "Der Vorname muss mindestens 2 Zeichen lang sein.";
+    } else if (!nameRegex.test(firstName)) {
+      return "Der Vorname darf nur Buchstaben und Bindestriche enthalten.";
+    }
+    return "";
+  }
+
+  const validateLastName = (lastName) => {
+    if (lastName === "") {
+      return "Bitte geben Sie Ihren Nachnamen ein.";
+    } else if (lastName.length < 2) {
+      return "Der Nachname muss mindestens 2 Zeichen lang sein.";
+    } else if (!nameRegex.test(lastName)) {
+      return "Der Nachname darf nur Buchstaben und Bindestriche enthalten.";
+    }
+    return "";
+  }
+
+  const validateEmail = (email) => {
+    if (email === "") {
+      return "Bitte geben Sie Ihre E-Mail-Adresse ein.";
+    } else if (!emailRegex.test(email)) {
+      return "Bitte geben Sie eine gültige E-Mail-Adresse ein.";
+    }
+    return "";
+  }
+
+  const validateMessage = (message) => {
+    if (message === "") {
+      return "Bitte geben Sie eine Nachricht ein.";
+    } else if (message.length < 2) {
+      return "Die Nachricht muss mindestens 2 Zeichen lang sein.";
+    } else if (!messageRegex.test(message)) {
+      return "Die Nachricht enthält ungültige Zeichen.";
+    }
+    return "";
+  }
+
+  const handleFirstNameChange = (event) => {
+    setFirstNameInput(event.target.value);
+   if (isFirstNameTouched) {
+      setFirstNameErrorMessage(validateFirstName(event.target.value));
+    }
+  }
+
+
+  const handleLastNameChange = (event) => {
+    setLastNameInput(event.target.value);
+    if (isLastNameTouched) {
+      setLastNameErrorMessage(validateLastName(event.target.value));
+    }
+  }
+
+  const handleEmailChange = (event) => {
+    setEmailInput(event.target.value);
+    if (isEmailTouched) {
+      setEmailErrorMessage(validateEmail(event.target.value));
+  }
+  }
+
+  const handleMessageChange = (event) => {
+    setMessageInput(event.target.value);
+    if (isMessageTouched) {
+      setMessageErrorMessage(validateMessage(event.target.value));
+    }
+}
+
+  const handleFirstNameBlur = () => {
+    setIsFirstNameTouched(true);
+    setFirstNameErrorMessage(validateFirstName(firstNameInput));
+  }
+
+  const handleLastNameBlur = () => {
+    setIsLastNameTouched(true);
+    setLastNameErrorMessage(validateLastName(lastNameInput));
+  }
+
+  const handleEmailBlur = () => {
+    setIsEmailTouched(true);
+    setEmailErrorMessage(validateEmail(emailInput));
+  }
+
+  const handleMessageBlur = () => {
+    setIsMessageTouched(true);
+    setMessageErrorMessage(validateMessage(messageInput));
+  }
+
   return (
     <section>
       <h1 className="text-5xl my-5 text-center" aria-label="Kontaktformular">
@@ -20,7 +157,7 @@ const Contact = () => {
         <label
           className="w-5/6 tablet:w-2/3 my-2 border text-left"
           id="first-name-label"
-          for="first-name"
+          htmlFor="first-name"
         >
           Vorname:
         </label>
@@ -32,11 +169,14 @@ const Contact = () => {
           required
           aria-labelledby="first-name-label"
           aria-required="true"
+          onChange={handleFirstNameChange}
+          onBlur={handleFirstNameBlur}
         />
+        <div className="w-5/6 tablet:w-2/3 my-2 text-left text-md text-red-800">{firstNameErrorMessage}</div>
         <label
           className="w-5/6 tablet:w-2/3 my-2 border  text-left"
           id="surname-label"
-          for="last-name"
+          htmlFor="last-name"
         >
           Nachname:
         </label>
@@ -48,8 +188,12 @@ const Contact = () => {
           required
           aria-labelledby="surname-label"
           aria-required="true"
+          onChange={handleLastNameChange}
+          onBlur={handleLastNameBlur}
         />
-        <label className="w-5/6 tablet:w-2/3 my-2 border text-left" for="email">
+        <div className="w-5/6 tablet:w-2/3 my-2 text-left text-md text-red-800">{lastNameErrorMessage}</div>
+
+        <label className="w-5/6 tablet:w-2/3 my-2 border text-left" htmlFor="email">
           E-Mail:
         </label>
         <input
@@ -59,10 +203,13 @@ const Contact = () => {
           name="email"
           required
           aria-required="true"
+          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
         />
+        <div className="w-5/6 tablet:w-2/3 my-2 text-left text-md text-red-800">{emailErrorMessage}</div>
         <label
           className="w-5/6 tablet:w-2/3 my-2 border text-left"
-          for="message"
+          htmlFor="message"
         >
           Nachricht:
         </label>
@@ -73,9 +220,12 @@ const Contact = () => {
           rows="10"
           required
           aria-required="true"
+          onChange={handleMessageChange}
+          onBlur={handleMessageBlur}
         ></textarea>
+        <div className="w-5/6 tablet:w-2/3 my-2 text-left text-md text-red-800">{messageErrorMessage}</div>
         <div className="w-5/6 tablet:w-2/3 my-2 border text-right">
-          <Button text="Absenden" link="#" aria="Button zum Absenden" />
+          <Button text="Absenden" link="#" aria="Button zum Absenden" formValid={isFormValid}/>
         </div>
       </form>
     </section>
